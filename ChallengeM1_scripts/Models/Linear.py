9362843752373913f64cbeace1640e703f1a9522
aleksanyan-volodya <volodya.aleksanyan13@gmail.com>
2026-01-20 13:54:38 +0100

    test for simple linear reg OK. The class wrokds well, and we need to do a analyse on how well is linReg for train/val data

--- ChallengeM1_scripts/Models/Linear.py
+++ ChallengeM1_scripts/Models/Linear.py
@@ -8,20 +8,19 @@ import numpy as np
 
 class LinearRegression:
     """Linear model"""
-    def __init__(self, loss_function = None, learning_rate=0.01, maxIter=1000):
-        self.loss_function = loss_function
-        # use score.py for loss functions
+    def __init__(self, learning_rate=0.01, maxIter=1000):
         self.learning_rate = learning_rate
         self.maxIter = maxIter
         self.weights = None
         self.bias = None 
+        self.errors = []
     
-    def __ones_trick(self, X):
-        """
-        Adds a colomn of 1 in X for bias
-        """
-        N = X.shape[0]
-        return np.hstack((np.ones((N, 1)), X))
+    # def __ones_trick(self, X):
+    #     """
+    #     Adds a colomn of 1 in X for bias
+    #     """
+    #     N = X.shape[0]
+    #     return np.hstack((np.ones((N, 1)), X))
 
     # def __gradient(self, X, y, y_pred, N):
     #     """
@@ -29,37 +28,33 @@ class LinearRegression:
     #     """
     #     return -2/N * np.dot(X.T, (y-y_pred)), -2 * np.mean()
 
-
-
     def fit(self, X, y):
         """
         Fitting the model returning the errorn, the weights, and f
         """
         # X = self.__ones_trick(X)
-        N, nb_param = X.shape
-        Err = []
-        F = []
-        # wieghts init
-        self.bias = 0 # No need with ones trick
-        self.weights = np.zeros(nb_param)
+        N, d = X.shape
+
+        self.bias = 0.0
+        self.weights = np.zeros(d)
 
         for i in range(self.maxIter):
-            Y_pred = np.dot(X, self.weights) + self.bias 
-            error = y - Y_pred
-            # rmse
-            mse = np.mean(error**2)
-            rmse = np.sqrt(mse)
+            y_pred = np.dot(X, self.weights) + self.bias 
+            error = y_pred - y
+
+            grad_w = (2 / N) * (X.T @ error)
+            grad_b = (2 / N) * np.sum(error)
+
+            self.weights -= self.learning_rate * grad_w
+            self.bias -= self.learning_rate * grad_b
+
+            rmse = np.sqrt(np.mean(error**2))
+            self.errors.append(rmse)
             
-            # Adding a small epsilon (1e-8) to avoid division by zero
-            grad = -(1 / (N * (rmse + 1e-8))) * (X.T @ error)
+            if rmse < 1e-6:
+                break
 
-            self.weights -= self.learning_rate * grad
-            self.bias -= self.learning_rate * (-2 * np.mean(error) / (rmse + 1e-8))
-
-            Err.append(rmse)
-            F.append(np.dot(X, self.weights) + self.bias)
-
-        return Err, F, self.weights
+        return self
     
     def predict(self, X):
         """
@@ -67,7 +62,3 @@ class LinearRegression:
         """
         return np.dot(X, self.weights) + self.bias
     
-
-
-
-
